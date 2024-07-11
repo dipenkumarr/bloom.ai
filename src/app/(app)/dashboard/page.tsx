aad683089fa3cd34abdde77108d4433cfc8731e5
Dipen Kumar <dipenbachani@gmail.com>
2024-07-11 02:41:04 -0600

    Add dashboard page main functionality

--- src/app/(app)/dashboard/page.tsx
+++ src/app/(app)/dashboard/page.tsx
@@ -1,6 +1,92 @@
-import React from "react";
+"use client";
+
+import { useToast } from "@/components/ui/use-toast";
+import { Message } from "@/model/User";
+import { acceptMessagesSchema } from "@/schemas/acceptMessageSchema";
+import { ApiResponse } from "@/types/ApiResponse";
+import { zodResolver } from "@hookform/resolvers/zod";
+import axios, { AxiosError } from "axios";
+import { useSession } from "next-auth/react";
+import { useCallback, useState } from "react";
+import { useForm } from "react-hook-form";
 
 const DashboardPage = () => {
+	const [messages, setMessages] = useState<Message[]>([]);
+	const [isLoading, setIsLoading] = useState(false);
+	const [isSwitchLoading, setIsSwitchLoading] = useState(false);
+
+	const { toast } = useToast();
+
+	const handleDeleteMessage = (messageId: string) => {
+		setMessages(messages.filter((message) => message._id !== messageId));
+	};
+
+	const { data: session } = useSession();
+
+	const form = useForm({
+		resolver: zodResolver(acceptMessagesSchema),
+	});
+
+	const { register, watch, setValue } = form;
+
+	const acceptMessages = watch("acceptMessages");
+
+	const fetchAcceptMessage = useCallback(async () => {
+		setIsSwitchLoading(true);
+
+		try {
+			const response = await axios.get<ApiResponse>(
+				"/api/accept-messages"
+			);
+			setValue("acceptMessages", response.data.isAcceptingMessage);
+		} catch (error) {
+			const axiosError = error as AxiosError<ApiResponse>;
+			toast({
+				title: "Error",
+				description:
+					axiosError.response?.data.message ||
+					"Failed to fetch message settings",
+				variant: "destructive",
+			});
+		} finally {
+			setIsSwitchLoading(false);
+		}
+	}, [setValue]);
+
+	const fetchMessages = useCallback(
+		async (refresh: boolean = false) => {
+			setIsLoading(true);
+			setIsSwitchLoading(false);
+
+			try {
+				const response = await axios.get<ApiResponse>(
+					"/api/get-messages"
+				);
+				setMessages(response.data.messages || []);
+
+				if (refresh) {
+					toast({
+						title: "Refreshed Messages",
+						description: "Showing all latest messages",
+					});
+				}
+			} catch (error) {
+				const axiosError = error as AxiosError<ApiResponse>;
+				toast({
+					title: "Error",
+					description:
+						axiosError.response?.data.message ||
+						"Failed to fetch latest messages",
+					variant: "destructive",
+				});
+			} finally {
+				setIsLoading(false);
+				setIsSwitchLoading(false);
+			}
+		},
+		[setIsLoading, setMessages]
+	);
+
 	return <div>DashboardPage</div>;
 };
 
